61735f748ce6436269838dbdaedf232300c3c342
odholden <odholden@gmail.com>
2015-10-09 13:10:48 +0100

    Completed logic for selecting random numbers for sections

--- js/app.js
+++ js/app.js
@@ -1,22 +1,23 @@
-//PSEUDO CODE - LOGIC FOR 'MAKE THE MONDRIAN'
+$(function(){
 
-//OBJECT OF THE GAME
-//A completed mondrian painting flashes up for 1 seconds, with red, blue and yellow blocks on a white background. Two human players take it in turns to complete the painting by trying to put the right colours in the right boxes. The player who completes the most correct paintings wins the game.
+  makeStartingBlocks();
 
-//LOGIC THOUGHTPATH
-//A page comes up with instructions on how to play. When the player presses a button, he is shown the first empty painting. 
-//On clicking the 'show' button, the player is shown the correct colours for 1 second. 
-//The player then has to click the squares to give them the red colour. Any square can be clicked and unclicked. 
-//The player clicks a button to confirm he is finished with red, and repeats the selection for blue and yellow. 
-//If the correct elements have had their colours changed, the player gains a point.
-//The turn then changes and a new board appears, and player two repeats the process.
-//Once each player has had three turns, the game ends and the player with the most points wins, or it is a tie.
-//The game can then be reset and played again.
+  function makeStartingBlocks() {
+    var startingBlocks = $('li').toArray();
+    getRandomNumbers();
+    sortRandomNumbers(randomNumbers);
+  }
 
-//potentially add a sandbox mode?
+  function getRandomNumbers() {
+    var randomNumbers  = [];
+    for (var i = 0; i < 6; i++) {
+      randomNumbers.push(Math.floor(Math.random()*24));
+    }
+    return randomNumbers;
+  }
 
+  function sortRandomNumbers(randomNumbers) {
 
-//app for design inspiraton  http://nizoapp.com/
-  //bg color #deddd8
-  //font "proxima-nova-1", "proxima-nova-2", Helvetica, sans-serif
-  //potential text color (as well as white): #888
+  }
+
+})
