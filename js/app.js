24d8fd3551846c0ce51fa9ab38ac8f8b20ab634e
odholden <odholden@gmail.com>
2015-10-09 23:56:20 +0100

    created button element to initialise filling the grid

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 $(function(){
 
-  var gridSize    = $('li').length;
-  makeGrid(gridSize);
+  var gridSize = $('li').length;
+  $('li').css("background-color", "white");
+  $('button').on("click", makeGrid(gridSize));
 
   function makeGrid(gridSize) {
     var blocks = makeBlocks();
     var colors = getColors();
-    console.log(colors);
     var block0 = fillGrid(blocks[0], colors[0]);
     var block1 = fillGrid(blocks[1], colors[1]);
     var block2 = fillGrid(blocks[2], colors[2]);
@@ -17,17 +17,17 @@ $(function(){
 
   function makeBlocks() {
     var blockLimits = getBlockLimits();
-    var block0 = getBlock(-1, blockLimits[0]);
-    var block1 = getBlock(blockLimits[0], blockLimits[1]);
-    var block2 = getBlock(blockLimits[1], blockLimits[2]);
-    var block3 = getBlock(blockLimits[2], blockLimits[3]);
-    var block4 = getBlock(blockLimits[3], blockLimits[4]);
-    var block5 = getBlock(blockLimits[4], gridSize - 1);
-    return blocks = [block0, block1, block2, block3, block4, block5];
+    var block0      = getBlock(-1, blockLimits[0]);
+    var block1      = getBlock(blockLimits[0], blockLimits[1]);
+    var block2      = getBlock(blockLimits[1], blockLimits[2]);
+    var block3      = getBlock(blockLimits[2], blockLimits[3]);
+    var block4      = getBlock(blockLimits[3], blockLimits[4]);
+    var block5      = getBlock(blockLimits[4], gridSize - 1);
+    return blocks   = [block0, block1, block2, block3, block4, block5];
   }
 
   function getColors() {
-    var colors = ["#EF5350", "#FFA726", "#42A5F5", "#66BB6A", "#FFEE58", "white"];
+    var colors         = ["#EF5350", "#FFA726", "#42A5F5", "#66BB6A", "#FFEE58", "white"];
     var shuffledColors = shuffle(colors);
     return shuffledColors;
   }
@@ -55,8 +55,8 @@ $(function(){
     var startingBlocks = [];
     for (var i = 1; i <= gridSize - 2; i++) {startingBlocks.push(i)}
     var shuffledBlocks = shuffle(startingBlocks);
-    var randomBlocks = startingBlocks.splice(3, 5);
-    var blockLimits = sortRandomBlocks(randomBlocks);
+    var randomBlocks   = startingBlocks.splice(3, 5);
+    var blockLimits    = sortRandomBlocks(randomBlocks);
     return blockLimits;
   }
 
