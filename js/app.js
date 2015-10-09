54b7405207a785fa8fd7e081de7f5b7dabe92749
odholden <odholden@gmail.com>
2015-10-09 13:49:39 +0100

    changed the way i generate the random numbers

--- js/app.js
+++ js/app.js
@@ -5,18 +5,24 @@ $(function(){
   function makeStartingBlocks() {
     var startingBlocks = $('li').toArray();
     var randomNumbers  = getRandomNumbers();
+    var orderedNumbers = sortRandomNumbers(randomNumbers);
   }
 
   function getRandomNumbers() {
     var randomNumbers  = [];
-    for (var i = 0; i < 6; i++) {
+    for (var i = 0; i < 5; i++) {
       randomNumbers.push(Math.floor(Math.random()*24));
     }
     return randomNumbers;
   }
 
   function sortRandomNumbers(randomNumbers) {
+    randomNumbers.sort(function(a,b) {
+      return a-b;
+    });
+    randomNumbers.push(24);
+    console.log(randomNumbers);
 
   }
 
-})
+});
