d89a2c770569cece2d7d0eb3db9cfd703012c4e8
odholden <odholden@gmail.com>
2015-10-09 11:20:36 +0100

    Created directories and files, imported prework from classwork/w02d04

---
+++ js/app.js
@@ -0,0 +1,22 @@
+//PSEUDO CODE - LOGIC FOR 'MAKE THE MONDRIAN'
+
+//OBJECT OF THE GAME
+//A completed mondrian painting flashes up for 1 seconds, with red, blue and yellow blocks on a white background. Two human players take it in turns to complete the painting by trying to put the right colours in the right boxes. The player who completes the most correct paintings wins the game.
+
+//LOGIC THOUGHTPATH
+//A page comes up with instructions on how to play. When the player presses a button, he is shown the first empty painting. 
+//On clicking the 'show' button, the player is shown the correct colours for 1 second. 
+//The player then has to click the squares to give them the red colour. Any square can be clicked and unclicked. 
+//The player clicks a button to confirm he is finished with red, and repeats the selection for blue and yellow. 
+//If the correct elements have had their colours changed, the player gains a point.
+//The turn then changes and a new board appears, and player two repeats the process.
+//Once each player has had three turns, the game ends and the player with the most points wins, or it is a tie.
+//The game can then be reset and played again.
+
+//potentially add a sandbox mode?
+
+
+//app for design inspiraton  http://nizoapp.com/
+  //bg color #deddd8
+  //font "proxima-nova-1", "proxima-nova-2", Helvetica, sans-serif
+  //potential text color (as well as white): #888
