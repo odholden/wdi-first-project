47b1eee9b7c82bc05236423c577df81a57a09912
odholden <odholden@gmail.com>
2015-10-09 22:22:40 +0100

    refactored code so gridsize can be adapted

--- js/app.js
+++ js/app.js
@@ -1,36 +1,31 @@
 $(function(){
 
-  var blocks    = makeBlocks();
-  var colorGrid = fillGrid(blocks);
+  var gridSize    = $('li').length;
+  var blocks      = makeBlocks();
+  var colorGrid   = fillGrid(blocks);
 
   function fillGrid(blocks) {
     var redBlock = blocks[0];
-    console.log(redBlock);
     for (var i = 0; i < redBlock.length; i++) {
       $("li#"+redBlock[i]).css("background-color","red");       
     }
-    var greenBlock = blocks[1];
-    console.log(greenBlock);
+    var greenBlock = blocks[3];
     for (var i = 0; i < greenBlock.length; i++) {
       $("li#"+greenBlock[i]).css("background-color","green");       
     }
     var blueBlock = blocks[2];
-    console.log(blueBlock);
     for (var i = 0; i < blueBlock.length; i++) {
       $("li#"+blueBlock[i]).css("background-color","blue");       
     }
-    var whiteBlock = blocks[3];
-    console.log(whiteBlock);
+    var whiteBlock = blocks[1];
     for (var i = 0; i < whiteBlock.length; i++) {
       $("li#"+whiteBlock[i]).css("background-color","white");       
     }
     var yellowBlock = blocks[4];
-    console.log(yellowBlock);
     for (var i = 0; i < yellowBlock.length; i++) {
       $("li#"+yellowBlock[i]).css("background-color","yellow");       
     }
     var blackBlock = blocks[5];
-    console.log(blackBlock);
     for (var i = 0; i < blackBlock.length; i++) {
       $("li#"+blackBlock[i]).css("background-color","black");       
     }
@@ -43,14 +38,16 @@ $(function(){
     var block3 = getBlock(blockLimits[1], blockLimits[2]);
     var block4 = getBlock(blockLimits[2], blockLimits[3]);
     var block5 = getBlock(blockLimits[3], blockLimits[4]);
-    var block6 = getBlock(blockLimits[4], 24);
+    var block6 = getBlock(blockLimits[4], gridSize - 1);
 
     return blocks = [block1, block2, block3, block4, block5, block6];
 
   }
 
   function getBlockLimits() {
-    var startingBlocks = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23];
+    var startingBlocks = [];
+    for (var i = 1; i <= gridSize - 2; i++) {startingBlocks.push(i)}
+    console.log(startingBlocks);
     var shuffledBlocks = shuffle(startingBlocks);
     var randomBlocks = startingBlocks.splice(3, 5);
     var blockLimits = sortRandomBlocks(randomBlocks);
