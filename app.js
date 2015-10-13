bab88cc489fd5902f82bd26da7e75605123f9047
odholden <odholden@gmail.com>
2015-10-13 10:46:58 +0100

    added fade effects and reorganised directory for heroku

--- app.js
+++ app.js
@@ -12,27 +12,25 @@ var correctGrid,
     winner;
 
 $(function responsiveOverlay(){
-
-  $(window).resize(function() {
-    if (document.documentElement.clientWidth >= 960) {    
-      $('#overlay1').fadeIn("slow").html("<h2>welcome to blick.</h2><p>blick is a two player game. this is how it works.</p><p>a board of colors will appear for five seconds.</p><p>you must recreate it by adding color to the correct squares in a blank grid.</p><p>you have five seconds to add each color to the board.</p><p>each color will be shown in the red box.</p><p>each player has three turns.</p><p>choose the biggest grid you dare.</p><p>happy blicking.</p><div id='letsblick'>let's blick.</div>");
-      $('#overlay1').css("padding", "0 15px");
-      $('p').css("font-size", "14px");
-
-    } else if (document.documentElement.clientWidth < 960 && document.documentElement.clientWidth > 840){
-      $('#overlay1').html("<h2>welcome to blick.</h2><div id='letsblick'>let's blick.</div>");
-      $('#playerOneTurn').remove();
-      $('#playerTwoTurn').remove();
-    } else if (document.documentElement.clientWidth <= 840){
-      $('#overlay1').html("<h2>BLICK.</h2><div id='letsblick'>let's blick.</div>");
-      $('#overlay1').css("width", "51.5vw").css("height", "100%").css("left", "24.5%").css("top","0px");
-      $('#overlay0').remove();
-      $('#overlay2').remove();
-      $('#letsblick').css("font-size","12px").css("letter-spacing","0").css("width", "60px").css("border", "2px solid #78909C").css("height", "60px");
-      $('#playerOneTurn').remove();
-      $('#playerTwoTurn').remove();
-      $('#totalScores').remove();
-    }  }).resize();
+  if (document.documentElement.clientWidth >= 960) {    
+    $('#overlay1').fadeIn("slow").html("<h2>welcome to blick.</h2><p>blick is a two player game. this is how iworks.</p><p>a board of colors will appear for five seconds.</p><p>you must recreate it by adding coloto the correct squares in a blank grid.</p><p>you have five seconds to add each color to the board.</p><each color will be shown in the red box.</p><p>each player has three turns.</p><p>choose the biggest griyou dare.</p><p>happy blicking.</p><div id='letsblick'>let's blick.</div>");
+    $('#overlay1').css("padding", "0 15px");
+    $('p').css("font-size", "14px")
+  } else if (document.documentElement.clientWidth < 960 && document.documentElement.clientWidth > 840){
+    $('#overlay1').html("<h2>welcome to blick.</h2><div id='letsblick'>let's blick.</div>");
+    $('#playerOneTurn').remove();
+    $('#playerTwoTurn').remove();
+  } else if (document.documentElement.clientWidth <= 840){
+    $('#overlay1').html("<h2>BLICK.</h2><div id='letsblick'>let's blick.</div>");
+    $('#overlay1').css("width", "51.5vw").css("height", "51.5vw").css("left", "24.5%").css("top","30
+      px");
+    $('#overlay0').remove();
+    $('#overlay2').remove();
+    $('#letsblick').css("font-size","12px").css("letter-spacing","0").css("width", "60px").css("border", "2psolid #78909C").css("height", "60px");
+    $('#playerOneTurn').remove();
+    $('#playerTwoTurn').remove();
+    $('#totalScores').remove();
+  };
   $('#letsblick').on("click", setup);  
 });
 
@@ -67,6 +65,7 @@ function makeGridLarge() {
 
 function startGame() {
   clearBoard();
+  $('li').css("background-color","none");
   $('#color').removeClass();
   turn+= 0.5;
   if (turn % 1 != 0) {
@@ -104,8 +103,11 @@ function makeGrid() {
   for (var i = 0; i < colors.length; i++) fillGrid(blocks[i], colors[i]);
   var correctGrid = [];
   for (var i = 0; i < gridSize;i++) {
+    setTimeout(function () {
     correctGrid.push($('li#' + [i]).css("background-color"));
+    }, 700);
   }
+  console.log(correctGrid);
   return correctGrid;
 }
 
@@ -231,6 +233,8 @@ function storeSelections() {
   for (var i = 0; i < gridSize;i++) {
     selectedGrid.push($('li#' + [i]).css("background-color"));
   }
+  console.log(selectedGrid);
+
   return selectedGrid;
 }
 
