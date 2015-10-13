7b3db08f45e07f150cb90958bb37475c0ac7b74a
odholden <odholden@gmail.com>
2015-10-13 02:12:06 +0100

    went to bed

--- js/app.js
+++ js/app.js
@@ -17,7 +17,7 @@ $(function responsiveOverlay(){
     if (document.documentElement.clientWidth >= 960) {    
       $('#overlay1').fadeIn("slow").html("<h2>welcome to blick.</h2><p>blick is a two player game. this is how it works.</p><p>a board of colors will appear for five seconds.</p><p>you must recreate it by adding color to the correct squares in a blank grid.</p><p>you have five seconds to add each color to the board.</p><p>each color will be shown in the red box.</p><p>each player has three turns.</p><p>choose the biggest grid you dare.</p><p>happy blicking.</p><div id='letsblick'>let's blick.</div>");
       $('#overlay1').css("padding", "0 15px");
-      $('p').css("font-size", "16px");
+      $('p').css("font-size", "14px");
 
     } else if (document.documentElement.clientWidth < 960 && document.documentElement.clientWidth > 840){
       $('#overlay1').html("<h2>welcome to blick.</h2><div id='letsblick'>let's blick.</div>");
@@ -25,10 +25,10 @@ $(function responsiveOverlay(){
       $('#playerTwoTurn').remove();
     } else if (document.documentElement.clientWidth <= 840){
       $('#overlay1').html("<h2>BLICK.</h2><div id='letsblick'>let's blick.</div>");
-      $('#overlay1').css("width", "51.5vw").css("height", "51.5vw").css("left", "24.5%").css("top","30px");
+      $('#overlay1').css("width", "51.5vw").css("height", "100%").css("left", "24.5%").css("top","0px");
       $('#overlay0').remove();
       $('#overlay2').remove();
-      $('#letsblick').css("font-size","12px").css("letter-spacing","0").css("width", "70px").css("border", "2px solid #78909C");
+      $('#letsblick').css("font-size","12px").css("letter-spacing","0").css("width", "60px").css("border", "2px solid #78909C").css("height", "60px");
       $('#playerOneTurn').remove();
       $('#playerTwoTurn').remove();
       $('#totalScores').remove();
