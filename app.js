5821b1262e2ace4e49d5927324e424932bfa504b
odholden <odholden@gmail.com>
2016-01-12 17:42:43 +0000

    fixed small indentation and visual inaccuracies

--- app.js
+++ app.js
@@ -11,13 +11,13 @@ var correctGrid,
     turnCounter,
     winner;
 
-$(function responsiveOverlay(){                                                   //MENTION OVERLAY AND RESPONSIVE
+$(function responsiveOverlay(){    
 
   $('#overlay1').fadeIn("slow").html("<h2>welcome to blick.</h2><p>blick is a two player game. this is how it works.</p><p>a board of colors will appear for five seconds.</p><p>you must recreate it by adding color to the correct squares in a blank grid.</p><p>you have five seconds to add each color to the board.</p><p>each color will be shown in the top left box.</p><p>each player has three turns.</p><p>choose the biggest grid you dare.</p><p>happy blicking.</p><div id='letsblick'>let's blick.</div>");
+
   if (document.documentElement.clientWidth >= 960) {    
     $('#overlay1').css("padding", "0 15px");
     $('p').css("font-size", "12px");
-
   } else if (document.documentElement.clientWidth < 960 && document.documentElement.clientWidth > 840){
     $('p').css("font-size", "11px");
     $('#playerOneTurn').remove();
@@ -34,9 +34,7 @@ $(function responsiveOverlay(){                                                 
     $('p').css("font-size", "12px").css("padding","2px 20px");
     $('#playerOneScore').text("0");
     $('#playerTwoScore').text("0");
-
-    };
-
+  };
   $('#letsblick').on("click", setup);  
 });
 
@@ -52,7 +50,7 @@ function setup() {
   $('#start').on("click", startGame);
 }
 
-function makeGridSmall() {                                                        //MENTION GRIDSIZE CHANGES
+function makeGridSmall() {                                                        
   gridSize = 16;
   $('ul').html('<li id="0"></li><li id="15"></li><li id="14"></li><li id="13"></li><li id="1"></li><li id="2"></li><li id="11"></li><li id="12"></li><li id="4"></li><li id="3"></li><li id="10"></li><li id="9"></li><li id="5"></li><li id="6"></li><li id="7"></li><li id="8"></li>');
   $('li').css("width", "25%").css("height", "25%");
@@ -74,7 +72,7 @@ function startGame() {
   clearBoard();
   $('li').css("background-color","none");
   $('#color').removeClass();
-  turn+= 0.5;                                                                      //MENTION TURN COUNTER (0.5)
+  turn+= 0.5;                                                                      
   if (turn % 1 != 0) {
     player        = 'playerOneScore';
     turnCounter   = 'playerOneTurn';
@@ -146,10 +144,10 @@ function shuffle(array) {
       j = 0,
       temp;
   while (i--) {
-      j = Math.floor(Math.random() * (i+1));
-      temp = array[i];
-      array[i] = array[j];
-      array[j] = temp; 
+    j = Math.floor(Math.random() * (i+1));
+    temp = array[i];
+    array[i] = array[j];
+    array[j] = temp; 
   }
   return array;
 }
@@ -186,14 +184,14 @@ function fillGrid(blocks, background) {
 function selectColors() {
   removeColors();
   colorTimer();
-  $('li').on("click", function() {                                                 //MENTION COLOR SELECTION
+  $('li').on("click", function() {                                                 
     $(this).removeClass();
     $(this).toggleClass($('#color').attr('class'));
   });
 }
 
 function removeColors() {
-  var shadows = ['shadow1', 'shadow2', 'shadow3', 'shadow4', 'shadow5', 'shadow6']
+  var shadows = ['shadow1', 'shadow2', 'shadow3', 'shadow4', 'shadow5', 'shadow6'];
   shuffle(shadows);
   $('li.red').removeClass().addClass(shadows[0]);
   $('li.green').removeClass().addClass(shadows[1]);
@@ -233,7 +231,6 @@ function colorTimer() {
     $('#color').removeClass(colors[5]);
     $('#color').fadeIn('fast').addClass('clear');
     $('#infoText').text("times up.");
-
   }, 30000);
   setTimeout(storeSelections, 30000);
 }
@@ -270,9 +267,9 @@ function compareGrids(correctGrid, selectedGrid) {
     playerTwoScore = playerTwoScore + score;
     if (document.documentElement.clientWidth < 860) {
       $('#' + player).text(playerTwoScore);
-      } else {
-        $('#' + player).text("score: " + playerTwoScore);
-      }
+    } else {
+      $('#' + player).text("score: " + playerTwoScore);
+    }
   }
   $('#' + turnCounter).text("turns taken: " + Math.ceil(turn));
   $('#infoText').text("you scored " + score + ".");
@@ -325,7 +322,6 @@ function resetGame() {
   $('#color').fadeIn('fast').addClass('clear');
   $('#playerOneScore').text('score: 0');
   $('#playerTwoScore').text('score: 0');
-
   turn = 0;
   correctGrid, selectedGrid, player, turnCounter, winner;
   clearBoard();
